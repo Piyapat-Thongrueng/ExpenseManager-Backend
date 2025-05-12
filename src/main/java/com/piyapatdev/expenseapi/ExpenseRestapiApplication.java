2482f1368c51cc5568eed93b847939e488de3e06
Piyapat <piyapat.thongrueng@gmail.com>
2025-05-12 11:15:24 +0700

    Create starter Spring Boot project

---
+++ src/main/java/com/piyapatdev/expenseapi/ExpenseRestapiApplication.java
@@ -0,0 +1,13 @@
+package com.piyapatdev.expenseapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseRestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseRestapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/piyapatdev/expenseapi/ExpenseRestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.piyapatdev.expenseapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseRestapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
