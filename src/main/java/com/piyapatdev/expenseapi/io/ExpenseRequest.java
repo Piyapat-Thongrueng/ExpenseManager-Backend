14f38fb456d22df030e238b07d5fe376c05690b3
Piyapat <piyapat.thongrueng@gmail.com>
2025-05-22 12:24:54 +0700

    completed save expense feature

--- src/main/java/com/piyapatdev/expenseapi/io/ExpenseRequest.java
+++ src/main/java/com/piyapatdev/expenseapi/io/ExpenseRequest.java
@@ -1,5 +1,8 @@
 package com.piyapatdev.expenseapi.io;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,14 +17,19 @@ import java.sql.Date;
 @Builder
 public class ExpenseRequest {
 
+    @NotBlank(message = "กรุณาระบุชื่อของค่าใช้จ่าย")
+    @Size(min = 3,message = "ชื่อของค่าใช้จ่ายต้องมีตัวอักษรอย่างน้อย 3 ตัว")
     private String name;
 
     private String note;
 
+    @NotBlank(message = "กรุณาระบุประเภทของค่าใช้จ่าย")
     private String category;
 
+    @NotNull(message = "กรุณาระบุวันที่ของค่าใช้จ่าย")
     private Date date;
 
+    @NotNull(message = "กรุณาระบุจำนวนของค่าใช้จ่าย")
     private BigDecimal amount;
 
 }
